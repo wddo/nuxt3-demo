21bea314a8b4f380804ea7cfb5aa7e468c3069bf
wddo <wddoddo@gmail.com>
2023-03-01 14:05:08 +0900

    vision

---
+++ pages/vision.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>vision</div>
+  <div>{{ first.title }}</div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useAsyncData("mountains", () =>
+  $fetch("https://api.nuxtjs.dev/mountains")
+);
+
+const list = ref<any>(data.value);
+
+const first = list.value.filter((v: object, i: number) => i === 0)[0];
+
+useHead({
+  title: first.title,
+});
+</script>
+
+<style lang="scss" scoped></style>
