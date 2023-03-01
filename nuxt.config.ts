67009b5c86b38f9d010ddfff6969ce5736ec07ee
wddo <wddoddo@gmail.com>
2023-03-01 19:37:20 +0900

    app.head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,36 @@ export default defineNuxtConfig({
   },
   app: {
     baseURL: "/nuxt3-demo/",
+    head: {
+      title: "nuxt template",
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "소개 문구",
+        },
+        {
+          name: "title",
+          content: "타이틀",
+        },
+        {
+          name: "keywords",
+          content: "키워드",
+        },
+        {
+          name: "author",
+          content: "jiwoo.kim",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
   },
   generate: {
     routes: ["/users/1", "/users/2", "/users/3"],
